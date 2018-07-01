c82cca8e45e34de18f0ea8d7c397028d15d67f41
Kamba Abudu <kabudu@gmail.com>
2018-07-01 14:54:51 +0100

    service configuration fix

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -19,7 +19,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('bundle_config');
+        $rootNode = $treeBuilder->root('custom');
 
         $rootNode
             ->children()
